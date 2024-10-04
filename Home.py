51ae249fe85274d291f2a64fa7585e64613b29e2
automacaotehokas <eric.martins@tehokas.com.br>
2024-10-04 13:41:46 -0300

    A

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Proposta Automatizada - Média Tensão",layout="wide")
 
 # Adicionando a imagem do logo
-st.image("image1png.png", width=200)
+st.image("image1png.png", width=100)
 
 # Título da página
 st.title("Proposta Automatizada - Média Tensão")
