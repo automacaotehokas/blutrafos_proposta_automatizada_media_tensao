c82c69987a5bad674cd1af1c7cb9035ba54cdeb4
automacaotehokas <eric.martins@tehokas.com.br>
2024-10-04 13:57:14 -0300

    A

--- Home.py
+++ Home.py
@@ -9,15 +9,23 @@ st.image("image1png.png", width=100)
 # Título da página
 st.title("Proposta Automatizada - Média Tensão")
 
-# Texto base descrevendo o programa
-st.write("""
-Bem-vindo à Proposta Automatizada de Média Tensão da Blutrafos. Este sistema foi desenvolvido para 
-facilitar o processo de criação de propostas comerciais personalizadas. Com ele, é possível configurar 
-itens técnicos, calcular preços e gerar documentos profissionais de maneira automatizada, otimizando 
-tempo e garantindo precisão nas informações fornecidas aos nossos clientes.
-
-Nosso objetivo é proporcionar uma solução eficiente, rápida e segura, integrando todas as etapas do 
-processo de criação de propostas em um único lugar, com a capacidade de armazenar e gerenciar dados de 
-forma centralizada através da integração com o SharePoint.
-""" ,
-    unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .justified-text {
+        text-align: justify;
+    }
+    </style>
+    <div class="justified-text">
+        Bem-vindo à Proposta Automatizada de Média Tensão da Blutrafos. Este sistema foi desenvolvido para 
+        facilitar o processo de criação de propostas comerciais personalizadas. Com ele, é possível configurar 
+        itens técnicos, calcular preços e gerar documentos profissionais de maneira automatizada, otimizando 
+        tempo e garantindo precisão nas informações fornecidas aos nossos clientes.
+        <br><br>
+        Nosso objetivo é proporcionar uma solução eficiente, rápida e segura, integrando todas as etapas do 
+        processo de criação de propostas em um único lugar, com a capacidade de armazenar e gerenciar dados de 
+        forma centralizada através da integração com o SharePoint.
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
