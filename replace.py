ffe7cdc57e2c9a0789954ece0ba6503d08ec19ae
automacaotehokas <eric.martins@tehokas.com.br>
2024-10-04 15:12:02 -0300

    a

--- replace.py
+++ replace.py
@@ -215,17 +215,16 @@ def create_custom_table_escopo(doc, itens_configurados):
 
         try:
             # Tenta converter a tensão secundária para float
+            # Convertendo a tensão secundária para float
             tensao_secundaria_float = float(tensao_secundaria_str)
-        
-            # Realiza o cálculo da tensão secundária dividida por 3 e pela raiz quadrada da tensão secundária
-            # Cálculo correto da tensão secundária a partir da tensão de linha (usando sqrt(3))
+
             # Cálculo correto da tensão secundária a partir da tensão de linha (usando sqrt(3))
             raiz_tensao_secundaria = math.sqrt(3)
             tensao_calculada = tensao_secundaria_float / raiz_tensao_secundaria
-            tensao_calculada_arredondada = round(tensao_calculada)  # Arredonda para o inteiro mais próximo
 
-            # Também arredondando o valor de tensao_secundaria_float para o inteiro mais próximo
-            tensao_secundaria_arredondada = round(tensao_secundaria_float)
+            # Forçando o arredondamento para cima (próximo número inteiro)
+            tensao_calculada_arredondada = math.ceil(tensao_calculada)  # Arredonda sempre para cima
+            tensao_secundaria_arredondada = math.ceil(tensao_secundaria_float)  # Arredonda sempre para cima
 
             # Formata o texto com os dois valores arredondados
             tensao_secundaria_texto = f"{tensao_secundaria_arredondada}V/{tensao_calculada_arredondada}V"
