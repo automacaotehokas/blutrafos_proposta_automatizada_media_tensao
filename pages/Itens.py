ed3af49b3972c93e5b2432a30c035b4d2ad852bd
Tehokas Soluções <eric.martins@tehokas.com.br>
2024-10-09 15:36:03 -0300

    api cidade

--- pages/Itens.py
+++ pages/Itens.py
@@ -77,7 +77,7 @@ if 'comissao' not in st.session_state:
 
 def obter_cidades():
     # Fazendo a requisição para a API da BrasilAPI para obter todos os municípios
-    url = "https://brasilapi.com.br/api/ibge/municipios/v1"
+    url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios"
     try:
         response = requests.get(url)
         response.raise_for_status()  # Levanta um erro para status de resposta HTTP 4xx/5xx
